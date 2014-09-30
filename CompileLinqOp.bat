2d8186b4740a403937655f0a77b38951163e43db
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2014-09-30 16:40:28 -0400

    adding in compilation batch file
    updating some of the linqop from the many places it was referenced

---
+++ CompileLinqOp.bat
@@ -0,0 +1,2 @@
+rem CompileLinqOp
+csc /out:LinqOp.dll /target:library LinqOp.cs
--- LinqOp.cs
+++ LinqOp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+
 using System.Reflection;
 
 // https://github.com/ImaginaryDevelopment/LinqOp
@@ -48,6 +50,7 @@ namespace ImaginaryDevelopment.Helpers
 
             /// <summary>
             /// For getting a static property name, for instance properties use the other overload   
+            /// Usage Example: LinqOp.PropertyOf(() => request.Foo).Name)
             /// </summary>
             public static PropertyInfo PropertyOf<T>(Expression<Func<T>> expression)
             {
@@ -56,17 +59,17 @@ namespace ImaginaryDevelopment.Helpers
                 return (PropertyInfo)body.Member;
             }
 
-            static Expression MaybeUnary<T>(Expression<T> exp)
-            {
-                Expression result;
-                var uExp = exp.Body as UnaryExpression;
-                result = uExp != null ? uExp.Operand : exp.Body;
-                return result;
-            }
             /// <summary>
-            /// Store a simple reference to your linqop delegate for reuse throughout the class or view
+            /// Usage existing.GetItemProperty(e => e.Bar).Name
             /// </summary>
-            /// <typeparam name="T"></typeparam>
+            public static PropertyInfo GetItemProperty<T>(this IEnumerable<T> items, Expression<Func<T, object>> expr)
+            {
+                return PropertyOf<T>(expr);
+            }
+
+            /// <summary>
+            /// Store a simple reference to your LinqOp delegate for reuse throughout the class (or any scope)
+            /// </summary>
             /// <returns></returns>
             public static Func<Expression<Func<T, object>>, string> PropertyNameHelper<T>()
             {
@@ -86,6 +89,54 @@ namespace ImaginaryDevelopment.Helpers
                 var body = (MemberExpression)expression.Body;
 
                 return (FieldInfo)body.Member;
+            }
+
+            public static MethodInfo GetActionInfoOf<T>(Expression<Action<T>> expression)
+            {
+                MethodCallExpression body = (MethodCallExpression)expression.Body;
+                return body.Method;
+            }
+
+            public class BindingPropertyResult
+            {
+                public string BindingName { get; set; }
+                public string SimpleName { get; set; }
+
+                public PropertyInfo MemberPropertyInfo { get; set; }
+                public PropertyInfo BindingPropertyInfo { get; set; }
+
+                public bool IsNested { get; set; }
+            }
+
+            public static BindingPropertyResult BindingPropertyOf<T>(Expression<Func<T>> expression)
+            {
+                MemberExpression body = expression.Body as MemberExpression;
+                if (body == null)
+                {
+                    UnaryExpression ubody = (UnaryExpression)expression.Body;
+                    body = (MemberExpression)ubody.Operand;
+                }
+
+                MemberExpression superExpression = (MemberExpression)body.Expression;
+                string superName = superExpression.Member.Name;
+                PropertyInfo superProp = superExpression.Member as PropertyInfo;
+
+                return new BindingPropertyResult()
+                {
+                    BindingName = superName + "." + body.Member.Name,
+                    BindingPropertyInfo = superProp,
+                    IsNested = superProp != null,
+                    MemberPropertyInfo = body.Member as PropertyInfo,
+                    SimpleName = body.Member.Name
+                };
+            }
+
+            static Expression MaybeUnary<T>(Expression<T> exp)
+            {
+                Expression result;
+                var uExp = exp.Body as UnaryExpression;
+                result = uExp != null ? uExp.Operand : exp.Body;
+                return result;
             }
         }
     }
